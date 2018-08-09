18f9fc4c8af69b62a6e1c8570b3f5700512fbc60
Aleksi Asikainen <aleksi.asikainen@solera.com>
2018-08-09 09:54:01 -0500

    Updated dependencies

--- custom_components/control4/__init__.py
+++ custom_components/control4/__init__.py
@@ -25,7 +25,7 @@ CONFIG_SCHEMA = vol.Schema({
 DATA_CONTROL4 = 'control4'
 DATA_CONTROL4_CONFIG = 'control4_config'
 
-REQUIREMENTS = ['python-control4-lite===0.1.6']
+REQUIREMENTS = ['python-control4-lite===0.1.7']
 
 
 async def async_setup(hass, config):
